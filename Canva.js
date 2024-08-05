1b67c58ea59c701eee5cb958b450dd5862fc73ff
AvatarAstam <91428844+AvatarAstam@users.noreply.github.com>
2024-08-05 22:21:30 +0530

    Update Canva.js

--- Canva.js
+++ Canva.js
@@ -7,12 +7,14 @@ import TextTool from './TextTool';
 import StickyNote from './StickyNote';
 import ImageUploader from './ImageUploader';
 import PageNavigator from './PageNavigator';
+import StickyNoteItem from './StickyNoteItem';
 
 const Canvas = () => {
   const canvasRef = useRef(null);
   const { 
     startDrawing, draw, stopDrawing, clearCanvas, undo, redo, 
-    setTool, setShape, addText, addStickyNote, updateStickyNotePosition, updateStickyNoteContent, addImage, updateImagePosition 
+    setTool, setShape, addText, addStickyNote, updateStickyNotePosition, updateStickyNoteContent, addImage, updateImagePosition,
+    pages, currentPage, addPage, deletePage, switchPage
   } = useCanvas(canvasRef);
   const { syncCanvas } = useRealtime(canvasRef);
 
@@ -21,7 +23,7 @@ const Canvas = () => {
     const context = canvas.getContext('2d');
     clearCanvas(context);
     syncCanvas(context);
-  }, [clearCanvas, syncCanvas]);
+  }, [clearCanvas, syncCanvas, currentPage]);
 
   return (
     <div className="canvas-container">
@@ -30,7 +32,13 @@ const Canvas = () => {
       <TextTool addText={addText} />
       <StickyNote addStickyNote={addStickyNote} />
       <ImageUploader addImage={addImage} />
-      <PageNavigator />
+      <PageNavigator 
+        pages={pages} 
+        currentPage={currentPage} 
+        addPage={addPage} 
+        deletePage={deletePage} 
+        switchPage={switchPage} 
+      />
       <canvas
         ref={canvasRef}
         width={800}
@@ -40,6 +48,25 @@ const Canvas = () => {
         onMouseUp={stopDrawing}
         onMouseLeave={stopDrawing}
       ></canvas>
+      {stickyNotes.map(note => (
+        <StickyNoteItem 
+          key={note.id} 
+          note={note} 
+          updatePosition={updateStickyNotePosition} 
+          updateContent={updateStickyNoteContent} 
+        />
+      ))}
+      {images.map(image => (
+        <img 
+          key={image.id} 
+          src={image.src} 
+          alt="uploaded"
+          style={{ top: image.y, left: image.x, position: 'absolute', cursor: 'move' }}
+          onMouseDown={(e) => startImageDrag(e, image.id)}
+          onMouseMove={(e) => dragImage(e, image.id)}
+          onMouseUp={stopImageDrag}
+        />
+      ))}
     </div>
   );
 };
