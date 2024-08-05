b3344f3b61d2b6e8ea0347d72173cea16b9a13e3
AvatarAstam <91428844+AvatarAstam@users.noreply.github.com>
2024-08-05 22:27:52 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// lib/firebase.js
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+
+export { db };
