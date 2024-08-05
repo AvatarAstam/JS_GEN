deb4e8accc31e8cf3fb6d3784842be2cd2c0ef6f
AvatarAstam <91428844+AvatarAstam@users.noreply.github.com>
2024-08-05 19:03:47 +0530

    Update Canvas.js 1232

--- Canvas.js
+++ Canvas.js
@@ -10,7 +10,7 @@ import PageNavigator from './PageNavigator';
 
 const Canvas = () => {
   const canvasRef = useRef(null);
-  const { startDrawing, draw, stopDrawing, clearCanvas, undo, redo, setTool, setShape } = useCanvas(canvasRef);
+  const { startDrawing, draw, stopDrawing, clearCanvas, undo, redo, setTool, setShape, addText } = useCanvas(canvasRef);
   const { syncCanvas } = useRealtime(canvasRef);
 
   useEffect(() => {
@@ -24,7 +24,7 @@ const Canvas = () => {
     <div className="canvas-container">
       <Toolbar setTool={setTool} />
       <ShapeTool setShape={setShape} />
-      <TextTool />
+      <TextTool addText={addText} />
       <StickyNote />
       <ImageUploader />
       <PageNavigator />
@@ -42,6 +42,7 @@ const Canvas = () => {
 };
 
 export default Canvas;
+
 
 
 
